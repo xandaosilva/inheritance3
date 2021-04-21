876c7f6af6302e80c5f4169eda67dfa1821bf7fd
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-04-21 12:05:32 -0300

    Add Entities, Enum and Program preview

---
+++ Inheritance3/Entities/Enums/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance3.Entities.Enums
+{
+    enum Color
+    {
+        BLACK = 0,
+        RED = 1,
+        BLUE = 2
+    }
+}
--- Inheritance3/Program.cs
+++ Inheritance3/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-
+using System.Globalization;
 namespace Inheritance3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Enter the number of shapes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Shape #{i + 1} data: ");
+                Console.Write("Rectangle or Circle (r/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Color (Black/Blue/Red): ");
+                string color = Console.ReadLine();
+
+                if (ch == 'r')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine(), CI);
+                    Console.Write("Height: ");
+                    double height = double.Parse(Console.ReadLine(), CI);
+                }
+                else
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CI);
+                }
+
+                Console.WriteLine("\nSHAPE AREAS:");
+                foreach ()
+                {
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
