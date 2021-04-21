8b1a48402f009b350049cfae2e0863e264e9f443
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-04-21 12:22:54 -0300

    Develop classess Circle and Rectangle

--- Inheritance3/Entities/Circle.cs
+++ Inheritance3/Entities/Circle.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Inheritance3.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Inheritance3.Entities
 {
-    class Circle
+    class Circle : Shape
     {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base(color)
+        {
+            Radius = radius;
+        }
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
     }
 }
--- Inheritance3/Entities/Rectangle.cs
+++ Inheritance3/Entities/Rectangle.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Inheritance3.Entities.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Inheritance3.Entities
 {
-    class Rectangle
+    class Rectangle : Shape
     {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
     }
 }
